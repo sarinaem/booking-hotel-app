118df8206fc8c5a8efed31c3830e6e056d332137
sarina <sarinaem75@gmail.com>
2024-08-02 17:31:18 +0330

    fix data for lat, lng

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -5,53 +5,46 @@ import { useSearchParams } from "react-router-dom";
 
 function Map() {
     const {isLoading, hotels} = useHotels(); // give data from hotels
-    const [mapCenter, setMapCenter] = useState([20, 4]);
+    const [mapCenter, setMapCenter] = useState([51, -3]);
     const [searchParams, setSearchParams] = useSearchParams();
+  
+
     const lat = searchParams.get("lat");
     const lng = searchParams.get("lng");
+    console.log(lat, lng);
 
-    useEffect(() => {
-        if(lat && lng) setMapCenter(lat, lng )
+
+      useEffect(() => {
+        if (lat && lng) setMapCenter({lat, lng});
       }, [lat, lng]);
-
+      console.log(lat, lng);
   return (
     <div className="mapContainer">
          <MapContainer className="map" 
-         center={mapCenter} 
+        //  center={mapCenter} 
+        center={{ lat: 51.505, lng: -0.09 }}
         // center={[lat || 50, lng || 3]}
-         zoom={13} 
+         zoom={10} 
          scrollWheelZoom={true}>
     <TileLayer
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
     />
-    <ChangeCenter 
-    position={mapCenter}
-    
-    />
-    {hotels.map((item) => {
-        return <Marker key={item.id} 
-        // position={[item.latitude, item.longitude]}>
-      position={[item.latitude, item.longitude]}>
-
-        <Popup>
-         {item.host_location}
-         
-        </Popup>
+    <ChangeCenter position={mapCenter} />
+    {hotels.map((item) => (
+       <Marker key={item.id} position={[item.latitude, item.longitude]}>
+        <Popup>{item.host_location}</Popup>
       </Marker>
-    }   
-)}
+   ))}
   </MapContainer>,
     </div>
-)
+);
 }
 
 export default Map
 
-
 function ChangeCenter({ position }) {
-    const map = useMap();
-    map.setView(position);
-    // console.log( map.setView(position));
-    return null;
+  const map = useMap();
+  map.setView(position);
+  return null;
 }
